db50cddda0ae39aa03c6db32630629456f1eb362
guy <guy5@post.bgu.ac.il>
2019-12-31 20:03:12 +0200

    Rename staff

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,9 @@
-# import pickle
-
 import numpy as np
-# import scipy.signal
 
 from maze_runner.models.model import Model
 
 
-def test_foo():
+def test_output():
     img = np.random.randn(28, 28)
     model = Model(fillters_number=10, dense_size=10, img_size=28)
     print(model.predict(img))
